66a20fa4fa85150d8b88de7a4f1596e2c97ace78
dpt5216 <dpt5216@psu.edu>
2022-02-05 14:24:22 -0500

    finished

--- RDParser.py
+++ RDParser.py
@@ -1,3 +1,8 @@
+####################################################################################
+#   Name:       Dean Taipale
+#   Date:       2/5/2022
+#   Purpose:    Implements a lexer and parser per the requirements of project 1
+####################################################################################
 
 INVALID, STRING, KEYWORD, EOI= -1, 0, 1, 2
 
@@ -16,9 +21,21 @@ class Token:
         if (self.type in [STRING, KEYWORD]):
             return self.value
         if (self.type == EOI):
-            return ""
+            return "EOI"
         else:
             return "Invalid"
+
+#   converts an integer type encoding (pseudo-enum) to a string
+def typeToString(type):
+    if (type == STRING):
+        return "string"
+    if (type == KEYWORD):
+        return "keyword"
+    if (type == EOI):
+        return "EOI"
+    if (type == INVALID):
+        return "invalid"
+
 
 
 LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -33,34 +50,32 @@ class Lexer:
         self.stmt = s
         self.index = 0
         self.nextChar()
+        self.isDone = False
 
     def nextToken(self):
         while True:
-            if (self.ch == '<'):# is a keyword
-                val = self.consumeChars("</")
-                if (self.ch in [LETTERS, DIGITS]):
-                    val += self.consumeChars(LETTERS+DIGITS)
-                    if (self.ch == '>'):
+            if (self.isDone): #are we at the end of the string?
+                return Token(EOI,"")
+            elif (self.ch == '<'):# is a keyword
+                val = self.consumeChars(KEYTAGS)
+                if (self.ch.isalpha()):
+                    val = val + self.consumeChars(LETTERS)
+                    if(self.ch == '>'):
+                        val = val + '>'
                         self.nextChar()
-                        val += '>'
                         return Token(KEYWORD, val)
-                    else:
-                        return Token(INVALID, val)
-                else:
-                    return Token(INVALID, val)
-
-            if (self.ch in [LETTERS, DIGITS]): # is a string
+            elif (self.ch.isalpha() or self.ch.isdigit()): # is a string
                 val = self.consumeChars(LETTERS+DIGITS)
                 return Token(STRING, val)
-
-            elif self.ch==' ': self.nextChar()
-            elif self.ch=='':
-                return Token(EOI,"")
-            else:
+            elif self.ch==' ': self.nextChar() # whitespace
+            else: # invalids
                 self.nextChar()
                 return Token(INVALID, self.ch)
 
-    def nextChar(self):
+    def nextChar(self): 
+        if (len(self.stmt) <= self.index): # if we are at the end of the list,
+            self.isDone = True # mark as done
+            return # don't walk off the end
         self.ch = self.stmt[self.index] 
         self.index = self.index + 1
 
@@ -72,15 +87,8 @@ class Lexer:
             self.nextChar()
         return r
 
-#    def checkChar(self, c):
-#        if (self.ch==c):
-#            self.nextChar()
-#            return True
-#        else: return False
 
 import sys
-
-# NOT YET IMPLEMENTED
 
 class Parser:
     def __init__(self, s):
@@ -88,51 +96,118 @@ class Parser:
         self.token = self.lexer.nextToken()
 
     def run(self):
-        self.statement()
+        self.webpage()
 
-    def statement(self):
-        print("<Statement>")
-        self.assignmentStmt()
-        while self.token.getTokenType() == SEMICOLON:
-            print("\t<Semicolon>;</Semicolon>")
+#   Implements: WEBPAGE -> <body> { TEXT } </body>
+    def webpage(self):
+        if (self.token.value != "<body>"):
+            self.errorStd("<body>",self.token.value)
+        print("<body>")
+        while (self.token.value != "</body>"):
             self.token = self.lexer.nextToken()
-            self.assignmentStmt()
-        self.match(EOI)
-        print("</Statement>")
+            self.text(1)
+            if (self.token.type == EOI):
+                self.errorTyp(KEYWORD,self.token.type)
+        print("</body>")
 
-    def assignmentStmt(self):
-        print("\t<Assignment>")
-        val = self.match(ID)
-        print("\t\t<Identifier>" + val + "</Identifier>")
-        self.match(ASSIGNMENTOP)
-        print("\t\t<AssignmentOp>:=</AssignmentOp>")
-        self.expression()
-        print("\t</Assignment>")
+#   Implements: TEXT -> STRING | <b> TEXT </b> | <i> TEXT </i> | LIST
+#   note: LIST is not a meaningful change in the grammar, just a useful abstraction for my code.
+    def text(self, indnts):
+        if(self.token.value == "</body>"):
+            return
+        elif(self.token.type == STRING):
+            print("  "*indnts + self.token.value)
+        elif(self.token.type == KEYWORD):
+            matchToken = ""
+            if (self.token.value == "<b>"):
+                matchToken = "</b>"
+            elif (self.token.value == "<i>"):
+                matchToken = "</i>"
+            elif (self.token.value == "<ul>"):
+                self.list(indnts)
+                return
+            print("  "*indnts + self.token.value)
+            self.token = self.lexer.nextToken()
+            self.text(indnts+1)
+            self.token = self.lexer.nextToken()
+            if(self.token.value != matchToken):
+                self.errorStd(matchToken, self.token.value)
+            print("  "*indnts + matchToken)
 
-    def expression(self):
-        if self.token.getTokenType() == ID:
-            print("\t\t<Identifier>" + self.token.getTokenValue() \
-                   + "</Identifier>")
-        elif self.token.getTokenType() == INT:
-            print("\t\t<Int>" + self.token.getTokenValue() + "</Int>")
-        elif self.token.getTokenType() == FLOAT:
-            print("\t\t<Float>" + self.token.getTokenValue() + "</Float>")
-        else:
-            print("Syntax error: expecting an ID, an int, or a float" \
-                  + "; saw:" \
-                  + typeToString(self.token.getTokenType()))
-            sys.exit(1)
+#   Implements: LIST -> <ul> { LISTITEM } </ul>
+#   note: LIST is not a meaningful change in the grammar, just a useful abstraction for my code.
+    def list(self, indnts):
+        if (self.token.value != "<ul>"):
+            self.errorStd("<ul>",self.token.value)
+        print("  "*indnts + self.token.value)
         self.token = self.lexer.nextToken()
-
-
-    def match (self, tp):
-        val = self.token.getTokenValue()
-        if (self.token.getTokenType() == tp):
+        while (self.token.value != "</ul>"):
+            self.listItem(indnts+1)
             self.token = self.lexer.nextToken()
-        else: self.error(tp)
-        return val
+            if (self.token.type == EOI):
+                self.errorTyp(KEYWORD,self.token.type)
+        print("  "*indnts + self.token.value)
 
-    def error(self, tp):
-        print ("Syntax error: expecting: " + typeToString(tp) \
-               + "; saw: " + typeToString(self.token.getTokenType()))
+#   Implements: LISTITEM -> <li> TEXT </li>
+    def listItem(self, indnts):
+        if (self.token.value != '<li>'):
+            self.errorStd('<li>', self.token.value)
+        print("  "*indnts + self.token.value)
+        self.token = self.lexer.nextToken()
+        self.text(indnts +1 )
+        self.token = self.lexer.nextToken()
+        if (self.token.value != '</li>'):
+            self.errorStd('</li>', self.token.value)
+        print("  "*indnts + self.token.value)
+        
+
+#   Error type for passing strings
+    def errorStd(self, exp, saw):
+        print ("Syntax error: expecting: " + exp \
+               + "; saw: " + saw )
         sys.exit(1)
+
+#   Error type for passing token types
+    def errorTyp(self, exp, saw):
+        print ("Syntax error: expecting: " + typeToString(exp) \
+               + "; saw: " + typeToString(saw) )
+        sys.exit(1)
+
+
+
+
+print("Testing the lexer: test 1")
+lex = Lexer ("<body> google <b><i><b> yahoo</b></i></b></body>")
+tk = lex.nextToken()
+while (tk.getType() != EOI):
+    print(tk)
+    tk = lex.nextToken()
+print("")
+
+print("Testing the lexer: test 2")
+lex = Lexer("<body> google <ul> <li> pizza </li> <li> apple </li> <li> bannana </li> </ul><i>pizza</i><b><i><b> yahoo</b></i></b></body>")
+tk = lex.nextToken()
+while (tk.getType() != EOI):
+    print(tk)
+    tk = lex.nextToken()
+print("")
+
+print("Testing the lexer: test 3")
+lex = Lexer("<body> google <ul> <li> pizza </li> <li> <b>apple </b> </li> <li> <i> <b>bannana</b> </i> </li> </ul><i> <b> pizza </b> </i><b><i><b> yahoo</b></i></b></body>")
+tk = lex.nextToken()
+while (tk.getType() != EOI):
+    print(tk)
+    tk = lex.nextToken()
+print("")
+
+print("Testing the parser: test 1")
+parser = Parser ("<body> google <b><i><b> yahoo</b></i></b></body>")
+parser.run()
+
+print("Testing the parser: test 2")
+parser = Parser ("<body> google <ul> <li> pizza </li> <li> apple </li> <li> bannana </li> </ul><i>pizza</i><b><i><b> yahoo</b></i></b></body>")
+parser.run()
+
+print("Testing the parser: test 3")
+parser = Parser("<body> google <ul> <li> pizza </li> <li> <b>apple </b> </li> <li> <i> <b>bannana</b> </i> </li> </ul><i> <b> pizza </b> </i><b><i><b> yahoo</b></i></b></body>")
+parser.run()
